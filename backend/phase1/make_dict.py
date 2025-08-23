e5338210a15568201979a362b89cb961a7190d15
Pouya Khoshnavazi <pouyanavazi@gmail.com>
2025-08-23 12:26:23 -0700

    Added JSON conversion of dictionary and blueprint of phase1 translation pipeline.

---
+++ backend/phase1/make_dict.py
@@ -0,0 +1,19 @@
+""" Module creates JSON dict
+    only needs to be run ONCE PER CONTAINTER INSTANCE """
+
+import json
+
+cmu_dict = {}   #DICT[WORD] = PHONEMES
+
+with open("cmudict.csv", "r") as file:          #read csv into DICT
+    for i in range(126):
+        next(file)
+
+    for line in file:
+        line = line.strip()
+        word, phonemes = line.split(maxsplit=1)
+        cmu_dict[word] = phonemes
+
+with open("dict.json", "w") as json_file:       #convert DICT to JSON for future use
+    json.dump(cmu_dict, json_file, indent=5)
+
---
+++ backend/phase1/translate.py
@@ -0,0 +1,20 @@
+""" Functionality to translate words to IPA """
+
+import json
+
+with open("dict.json") as file:
+    DICT = json.load(file)
+
+def convert_phonemes(phonemes):
+    """ Convert phonemes to IPA word """
+
+    return phonemes
+
+def translate_word(word):
+    """ Take English word and convert to IPA """
+
+    phonemes = DICT[word]
+    return convert_phonemes(phonemes)
+
+#TODO frontend checks if word provided is English
+#if word cannot be translated, activate phase2
