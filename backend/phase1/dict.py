c8ed9d39a2babb5153987c75a4e8adffac6867b3
Pouya Khoshnavazi <pouyanavazi@gmail.com>
2025-08-23 11:48:21 -0700

    Dictionary of word to phonemes completed. Read from cmudict csv.

--- backend/phase1/dict.py
+++ backend/phase1/dict.py
@@ -1,8 +1,15 @@
 """ Module contains dictionary and its functions """
 
-import csv
-
-DICT = {} #dict[WORD] = PHONEMES 
+#DICT[WORD] = PHONEMES
+DICT = {}
 
 with open("cmudict.csv", "r") as file:
-    
+    for i in range(126):
+        next(file)
+
+    for line in file:
+        line = line.strip()
+        word, phonemes = line.split(maxsplit=1)
+        DICT[word] = phonemes
+
+# Can wrangle and make dict permanent data structure if needed 
