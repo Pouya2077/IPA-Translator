36f14d80bc22c265c2600207012d0e103d54b6e2
Pouya Khoshnavazi <pouyanavazi@gmail.com>
2025-08-30 00:00:06 -0700

    Added special character removal to translation pipeline.

--- backend/phase1/translate.py
+++ backend/phase1/translate.py
@@ -14,12 +14,13 @@ with open(CMU_DICT_PATH) as file:
 with open(ARPABET_DICT_PATH) as file:
     ARPABET_DICT = json.load(file)
 
-def remove_stress(chars):
+def remove_specials(chars):
     """ Remove stress (numbers) from ARPABET chars """
     phonemes = []
 
     for char in chars:
         char = re.sub(r'\d', '', char)
+        char = re.sub(r'\W', '', char)
         phonemes.append(char)
 
     return phonemes
@@ -31,7 +32,7 @@ def convert_to_ipa(arpabet_char):
 def convert_phonemes(phonemes):
     """ Convert phonemes to IPA word """
     chars = phonemes.split(" ")
-    chars = remove_stress(chars)
+    chars = remove_specials(chars)
     ipa_word = ""
 
     for char in chars:
@@ -43,8 +44,21 @@ def translate_word(word):
     """ Convert English word to IPA """
 
     phonemes = CMU_DICT[word.lower()]
-    return "/" + convert_phonemes(phonemes) + "/"
+    return convert_phonemes(phonemes)
+
+def translate_sentence(sentence):
+    """ Convert sentence (entry beyond one word) into IPA """
+
+    ipa_sentence = ""
+    words = sentence.split(" ")
+    for word in words:
+        ipa_sentence = ipa_sentence + translate_word(word)
+
+    return f"/{ipa_sentence}/"
 
 #TODO frontend checks if word provided is English (latin alphabet)
 #TODO consider adding functionality to support punctuation
 #TODO if word cannot be translated, activate phase2
+
+print(translate_sentence("who likes free cake"))
+print(translate_word("doesn't"))
