04ac814e91395b085561fd2bf892ead152418acf
Pouya Khoshnavazi <pouyanavazi@gmail.com>
2025-08-30 21:21:20 -0700

    Wrote test cases for ARPABET json dict.

--- backend/tests/test_make_dict.py
+++ backend/tests/test_make_dict.py
@@ -1,9 +1,11 @@
 import os
 import json
+import csv
 
 BASE_PATH =         os.path.dirname(__file__)
 CMU_DICT_PATH =     os.path.join(BASE_PATH, "../phase1/cmu_dict.json")
 ARPABET_DICT_PATH = os.path.join(BASE_PATH, "../phase1/arpabet_dict.json")
+ARPABET_CSV_PATH =  os.path.join(BASE_PATH, "../phase1/arpabet.csv")
 
 with open(CMU_DICT_PATH) as file:
     CMU_DICT = json.load(file)
@@ -11,4 +13,12 @@ with open(CMU_DICT_PATH) as file:
 with open(ARPABET_DICT_PATH) as file:
     ARPABET_DICT = json.load(file)      #setting up dicts for tests to use
 
-# TODO write test cases
+def test_arpabet_dict_correct_values():
+    with open(ARPABET_CSV_PATH, "r") as file:
+        next(file)
+        reader = csv.reader(file)
+
+        for row in reader:
+            assert ARPABET_DICT[row[0]] == row[1]
+
+# TODO come back for cmu_dict testing
